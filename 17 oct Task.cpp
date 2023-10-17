d96a76261e364a23ae1d772548da4ca49f6342cd
badhonosd <147328322+badhonosd@users.noreply.github.com>
2023-10-17 21:39:35 +0600

    Add files via upload

---
+++ 17 oct Task.cpp
@@ -0,0 +1,113 @@
+
+//Mem, My aiub computer was Crashed.This Task upload by laptop.
+
+#include <iostream>
+
+using namespace std;
+
+
+const int size = 500;
+
+int A[size];
+
+int top = -1;
+
+
+bool isEmpty() {
+
+    return top == -1;
+
+}
+
+
+void push(int value) {
+
+    if (top == size - 1) {
+
+        cout << "Stack is full" << endl;
+
+    } else {
+
+        top++;
+
+        A[top] = value;
+
+    }
+
+}
+
+
+void pop() {
+
+    if (isEmpty()) {
+
+        cout << "Stack is empty" << endl;
+
+    } else {
+
+        top--;
+
+    }
+
+}
+
+
+void showTop() {
+
+    if (isEmpty()) {
+
+        cout << "Stack is empty" << endl;
+
+    } else {
+
+        cout << "Element at the top is: " << A[top] << endl;
+
+    }
+
+}
+
+
+void displayStack() {
+
+    if (isEmpty()) {
+
+        cout << "Stack is empty" << endl;
+
+    } else {
+
+        for (int i = 0; i <= top; i++) {
+
+            cout << A[i] << " ";
+
+        }
+
+        cout << endl;
+
+    }
+
+}
+
+
+int main() {
+
+    push(7);
+
+    push(8);
+
+    push(9);
+
+
+    displayStack();
+
+
+    showTop();
+
+
+    pop();
+
+    displayStack();
+
+
+    return 0;
+
+}
